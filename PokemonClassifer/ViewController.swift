0a344bb6c7bb72083681dbb24df8faeaed887be7
Joshua Yang <ybd1992@hotmail.com>
2019-12-26 21:18:20 -0800

    Initial Commit

---
+++ PokemonClassifer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonClassifer
+//
+//  Created by Joshua Yang on 12/26/19.
+//  Copyright © 2019 Joshua Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
